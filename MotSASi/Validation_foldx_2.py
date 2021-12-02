63124f82d4a72d13531224c5178268a4fe2c9f8f
Mariano Martín <70019972+martinmariano91@users.noreply.github.com>
2021-12-02 08:20:31 -0300

    Add files via upload

--- MotSASi/Validation_foldx_2.py
+++ MotSASi/Validation_foldx_2.py
@@ -7,37 +7,39 @@ Created on Tue Nov 23 08:30:41 2021
 """
 
 import pandas as pd
-import sys
+#import sys
 
 #mot = sys.argv[2]
 #motif_name = sys.argv[3]
 
 #motifs_names = os.listdir('../MotListos_1er_etapa/')
 
-motifs_names = [('DEG_APCC_KENBOX_2_motif', 'x.K.E.N.x'),
-                ('LIG_SUMO_SIM_anti_2_motif', 'DEST.x.x.VIL.DESTVILMA.VIL.VILM.x.DEST'),
-                ('LIG_PCNA_PIPBox_1_motif', 'QM.x.^FHWY.LIVM.^P.^PFWYMLIV.FYHL.FYWL.x.x'),
-                ('DOC_MAPK_JIP1_4_motif', 'RK.P.^P.^P.L.x.LIVMF'),
-                ('DOC_CYCLIN_RxL_1_motif_2', 'x.^EDWNSG.^D.RK.^D.L.FL.EDST'),
-                ('DOC_ANK_TNKS_1_motif', 'x.R.x.x.PGAV.DEIP.G.x'),
-                ('MOD_CDK_SPxK_1_motif', 'x.x.x.ST.P.x.KR'),
-                ('DOC_MAPK_NFAT4_5_motif', 'RK.^P.^P.LIM.x.L.x.LIVMF.x'),
+motifs_names = [('DOC_MAPK_JIP1_4_motif', 'RK.P.^P.^P.L.x.LIVMF'),
+                ('LIG_PCNA_PIPBox_1_motif', 'QM.x.^FHWY.LIVM.^P.^PFWYMLIV.FYHL.FYWL.x.x'),           
+                ('DOC_PP2A_B56_1_motif', 'LMFYWIC.x.x.IVLWC.x.E'),                
                 ('LIG_NRBOX_motif', '^P.L.^P.^P.L.L.^P'),
-                ('DOC_PP2A_B56_1_motif', 'LMFYWIC.x.x.IVLWC.x.E'),
-                ('LIG_PAM2_1_motif', 'x.x.LFP.NS.PIVTAFL.x.A.x.x.FYW.x.PYLF.x'),
+                ('DOC_MAPK_NFAT4_5_motif', 'RK.^P.^P.LIM.x.L.x.LIVMF.x'),
+                ('DEG_APCC_KENBOX_2_motif', 'x.K.E.N.x'),
+                ('DOC_ANK_TNKS_1_motif', 'x.R.x.x.PGAV.DEIP.G.x'),
+                ('LIG_PAM2_1_motif_1', 'x.x.LFP.NS.PIVTAFL.x.A.x.x.FY.x.PYLF.x'),	                
                 ('LIG_PTAP_UEV_1_motif', 'x.P.TS.A.P.x'),
-                ('LIG_CaM_IQ_9_motif_1', 'ACLIVTM.^P.^P.ILVMFCT.Q.^P.^P.^P.RK.^P.^P.^P.^P.RKQ.^P.^P'),
-                ('LIG_PDZ_Class_1_motif', 'x.x.x.ST.x.ACVILF'),
-                ('DOC_CYCLIN_RxL_1_motif_1', 'x.^EDWNSG.^D.RK.^D.L.FL.x.x.x.EDST'),
-                ('DOC_MAPK_MEF2A_6_motif', 'RK.x.x.x.LIVMP.x.LIV.x.LIVMF'),
-                ('LIG_SUMO_SIM_par_1_motif', 'DEST.x.VILPTM.VIL.DESTVILMA.VIL.DEST'),
                 ('LIG_SH3_2_motif', 'P.x.x.P.x.KR'),
-                ('LIG_CaM_IQ_9_motif_2', 'ACLIVTM.^P.^P.ILVMFCT.Q.^P.^P.^P.RK.^P.^P.^P.^P.^P.RKQ.^P.^P')]
+                ('MOD_CDK_SPxK_1_motif', 'x.x.x.ST.P.x.KR'),
+                ('DOC_MAPK_MEF2A_6_motif', 'RK.x.x.x.LIVMP.x.LIV.x.LIVMF'),                               
+                ('DOC_CYCLIN_RxL_1_motif_2', 'x.^EDWNSG.^D.RK.^D.L.FL.EDST'),
+                ('LIG_CaM_IQ_9_motif_1', 'ACLIVTM.^P.^P.ILVMFCT.Q.^P.^P.^P.RK.^P.^P.^P.^P.RKQ.^P.^P'),
+                ('LIG_CaM_IQ_9_motif_2', 'ACLIVTM.^P.^P.ILVMFCT.Q.^P.^P.^P.RK.^P.^P.^P.^P.^P.RKQ.^P.^P'),
+                ('LIG_PDZ_Class_1_motif', 'ST.x.VIL'),
+                ('TRG_diLeu_motif', 'EDQR.x.x.x.L.VIL')]
+                
+trues_list = []
+falses_list = []
+conflicts_list = []
+motifs_names_list = []
 
-dfs = []
 for motif_name_folder, mot in motifs_names:
-    print('*'*50)
-    print(motif_name_folder)
+    #print('*'*50)
+    #print(motif_name_folder)
     mot = mot.split('.')
     if motif_name_folder.endswith('motif'):
         motif_name = motif_name_folder[:-6]        
@@ -59,41 +61,29 @@ for motif_name_folder, mot in motifs_names:
         row_mean = foldx.iloc[i].mean()
         row_std = foldx.iloc[i].std()
         for e in foldx.columns:
-            if foldx.iloc[i][e] > row_mean: # patogenicos           
-                if i in pos_x:
+            if i in pos_x: # check if is a flexible position
+                # Most of the changes at this position will be benign
+                if foldx.iloc[i][e] > row_mean+row_std: 
+                    # if the ddG z-score is >1, the change is pathogenic
                     for x in d.values():
                         pathogenics.append(x+str(i+1)+d[e])
-                else: 
-                    aa = [x for x in mot[i] if x != '^']
-                    for z in aa:
-                        pathogenics.append(d[z]+str(i+1)+d[e])            
-            elif foldx.iloc[i][e] < row_mean: # benignos
-                if i in pos_x:
+                else:
+                    # if the ddG z-score is <1, the change is benign
                     for x in d.values():
                         benigns.append(x+str(i+1)+d[e])
+            else:                  
+                # Most of the changes at this position will be pathogenic
+                if foldx.iloc[i][e] > row_mean-row_std:
+                    # if the ddG z-score is >-1, the change is pathogenic
+                    aa = [x for x in mot[i] if x != '^']
+                    for z in aa:                    
+                        pathogenics.append(d[z]+str(i+1)+d[e])                                                    
                 else:             
+                    # if the ddG z-score is <-1, the change is benign
                     aa = [x for x in mot[i]]
                     for z in aa:
                         benigns.append(d[z]+str(i+1)+d[e])
-            #else:
-            #    row_mean = foldx.iloc[i].mean()
-            #    if foldx.iloc[i][e] > row_mean:
-            #        if i in pos_x:
-            #            for x in d.values():
-            #                pathogenics.append(x+str(i+1)+d[e])
-            #        else:
-            #            aa = [x for x in mot[i]]
-            #            for z in aa:
-            #                pathogenics.append(d[z]+str(i+1)+d[e])
-            #    else:
-            #        if i in pos_x:
-            #            for x in d.values():
-            #                benigns.append(x+str(i+1)+d[e])
-            #        else:
-            #            aa = [x for x in mot[i]]
-            #            for z in aa:
-            #                benigns.append(d[z]+str(i+1)+d[e])
-                            
+                    
     #%%
                             
     df = pd.read_csv('../MotListos_1er_etapa/'+motif_name_folder+'/'+motif_name+'_motif.csv', index_col=0, sep='\t')
@@ -130,16 +120,28 @@ for motif_name_folder, mot in motifs_names:
     
     conflict_motifs = true_motifs.loc[true_motifs.ID_motif.isin(falses), 'ID_motif'].tolist()+false_motifs.loc[false_motifs.ID_motif.isin(trues), 'ID_motif'].tolist()
     
-    with open('../MotListos_1er_etapa/'+motif_name_folder+'/'+motif_name+'_Validation_trues.txt', "w") as output:
+    trues_list.append(len(true_motifs))
+    falses_list.append(len(false_motifs))
+    conflicts_list.append(len(conflict_motifs))
+    motifs_names_list.append(motif_name)
+    
+    with open('../MotListos_1er_etapa/'+motif_name_folder+'/'+motif_name+'_Validation_trues2.txt', "w") as output:
         output.write(str(true_motifs['ID_motif'].tolist()))
         
-    with open('../MotListos_1er_etapa/'+motif_name_folder+'/'+motif_name+'_Validation_falses.txt', "w") as output:
+    with open('../MotListos_1er_etapa/'+motif_name_folder+'/'+motif_name+'_Validation_falses2.txt', "w") as output:
         output.write(str(false_motifs['ID_motif'].tolist()))
         
-    with open('../MotListos_1er_etapa/'+motif_name_folder+'/'+motif_name+'_Validation_conflicts.txt', "w") as output:
+    with open('../MotListos_1er_etapa/'+motif_name_folder+'/'+motif_name+'_Validation_conflicts2.txt', "w") as output:
         output.write(str(conflict_motifs))
     
-    print(f'Corroborated with FoldX Matrix:', len(true_motifs))
-    print(f'Discarded with FoldX Matrix:', len(false_motifs))
-    print(f'Conflicts (have some variants in accordance and some not):', len(conflict_motifs))
-                            
+    #print(f'Corroborated with FoldX Matrix:', len(true_motifs))
+    #print(f'Discarded with FoldX Matrix:', len(false_motifs))
+    #print(f'Conflicts (have some variants in accordance and some not):', len(conflict_motifs))
+    
+final = pd.DataFrame()
+final['Motif'] = motifs_names_list
+final['Trues'] = trues_list
+final['Falses'] = falses_list
+final['Conflicts'] = conflicts_list
+
+print(final)
